696a1de6c8a9373805215ef89c82fccbbeff4cc9
jmlee <jmlee90@gmail.com>
2025-04-22 18:22:27 +0900

    Initial commit

---
+++ src/main/java/supernova/learningportal/SupernovaLearningportalApplication.java
@@ -0,0 +1,13 @@
+package supernova.learningportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupernovaLearningportalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SupernovaLearningportalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/supernova/learningportal/SupernovaLearningportalApplicationTests.java
@@ -0,0 +1,13 @@
+package supernova.learningportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupernovaLearningportalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
